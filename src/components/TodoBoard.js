c28781564c2063f7d1fbf2f1298d26cdfe080b92
tjwls11 <166362199+tjwls11@users.noreply.github.com>
2024-07-03 00:20:13 +0900

    Update TodoBoard.js

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -1,15 +1,28 @@
-import React from "react";
-import TodoItem from "./TodoItem";
+import React from 'react';
+import TodoItem from './TodoItem';
 
-function TodoBoard(props){
-    console.log("todoBoard", props.todoList);
+function TodoBoard({ todoList, onDelete, onUpdate }) {
+    const handleUpdateEmoji = (index, newEmoji) => {
+        const updatedList = [...todoList];
+        updatedList[index].emoji = newEmoji;
+        onUpdate(updatedList);
+    };
 
     return (
-        <div>
-            <h1>My Todo List</h1>
-            {props.todoList.map((item, index) => (
-                <TodoItem key={index} item={item} onDelete={() => props.onDelete(index)} />
-            ))}
+        <div className="todo-board">
+            <h1 style={{ color: '#974D99' }}>My Todo List</h1> {/* 원하는 색상으로 변경 */}
+            {todoList.length === 0 ? (
+                <p>No items in the list</p>
+            ) : (
+                todoList.map((item, index) => (
+                    <TodoItem
+                        key={index}
+                        item={item}
+                        onDelete={() => onDelete(index)}
+                        onUpdateEmoji={(newEmoji) => handleUpdateEmoji(index, newEmoji)}
+                    />
+                ))
+            )}
         </div>
     );
 }
